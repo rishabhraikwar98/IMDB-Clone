4a7dd148b2743ea88e65a15fe507ad73adb374a5
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2022-11-18 17:04:06 +0530

    done final changes

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>IMDB clone/Movie List App Using React js </h1>
+<h3>Tools used in this projects are <h3>
+<ul>
+<li>React js</li>
+<li>themoviedb API</li>
+<li>Vs code</li>
+<li>Node/npm</li>
+</ul>
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/pages/home/Home";
+import MovieList from "./components/movieList/MovieList";
+import Movie from "./components/pages/movieDetail/Movie"
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route index element={<Home/>}></Route>
+          <Route path="movie/:id" element={<Movie/>}></Route>
+          <Route path="movies/:type" element = {<MovieList/>}></Route>
+          <Route path="/*" element ={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
